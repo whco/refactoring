4b51f7eec93e3bbdf3304b5a2c7a8ec359348936
heejae_shin <heejae_shin@tmax.co.kr>
2022-05-11 18:12:08 +0900

    Change Switch cause to ReDefinition : Move method to class where variable whose possibility of change is high

--- src/Movie.java
+++ src/Movie.java
@@ -17,4 +17,23 @@ public class Movie {
     public String getTitle (){
         return _title;
     };
+
+    double getCharge(int daysRented) {
+        //determine amounts for each line
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
